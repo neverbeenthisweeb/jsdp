c03bf45b51dcd6cd0ede8e804bc39b800dc44d68
Rafi Ramadhana <42462215+neverbeenthisweeb@users.noreply.github.com>
2021-08-22 00:28:12 +0700

    Add use case comments in decorator pattern

--- design_patterns/decorator/app.js
+++ design_patterns/decorator/app.js
@@ -1,3 +1,4 @@
+// Use Case:
 // We run a beverages business.
 // We sell Coca-Cola and Fanta.
 // We provide additional condiments.
@@ -5,6 +6,7 @@
 const component = require('./component.js');
 const decorator = require('./decorator.js');
 
+// prepare order 1
 let order1 = new component.CocaCola();
 order1 = new decorator.Chocolate(order1);
 order1 = new decorator.Chocolate(order1);
@@ -12,6 +14,7 @@ order1 = new decorator.Cheese(order1);
 console.log(`order1_description: ${order1.describe()}`);
 console.log(`order1_cost: ${order1.getCost()}`);
 
+// prepare order 2
 let order2 = new component.CocaCola();
 order2 = new decorator.Cheese(order2);
 order2 = new decorator.Cheese(order2);
